c1eac2c59c82214a5b681d63514e8cc6fde52442
bxgda <jovan.bogdanovic@elfak.rs>
2026-02-03 14:19:10 +0100

    feat(FileMetadata): klasa za metadata header definiciju

---
+++ src/Common/FileMetadata.cs
@@ -0,0 +1,36 @@
+namespace src.Common;
+
+public class FileMetadata
+{
+    public string FileName { get; set; } = string.Empty;
+
+    public long FileSize { get; set; }
+
+    public DateTime Timestamp { get; set; }
+
+    public string EncryptingAlgorithm { get; set; } = string.Empty;
+
+    public string? Mode { get; set; } = string.Empty;
+
+    public string HashValue { get; set; } = string.Empty;
+
+    public string ToJson()
+    {
+        return System.Text.Json.JsonSerializer.Serialize(this, new System.Text.Json.JsonSerializerOptions
+        {
+            WriteIndented = true
+        });
+    }
+
+    public static FileMetadata? FromJson(string json)
+    {
+        try
+        {
+            return System.Text.Json.JsonSerializer.Deserialize<FileMetadata>(json);
+        }
+        catch
+        {
+            return null;
+        }
+    }
+}
